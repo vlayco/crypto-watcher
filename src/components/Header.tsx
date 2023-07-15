16d92bb978cddad961d5e8815da7aa6f8aa1a64f
Vladimir Vlayco <vladaj00@gmail.com>
2023-07-15 12:22:05 +0200

    change of mind: removed react-select, added back good old default html select

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 const Header = () => {
+
   return (
     <div className="flex flex-row p-4 border-b border-yellow-300/20 mb-5 shadow-xl shadow-lime-200/5">
       <nav className="container mx-auto w-full flex flex-row items-center justify-between">
@@ -8,10 +9,19 @@ const Header = () => {
           <Link href="/">CryptoWatch</Link>
         </span>
 
-        <span className="text-xl text-lime-400 hover:text-lime-500 transition-colors">
-          <Link href="/">Login</Link>
-        </span>
-        
+        <div className="flex flex-row items-center justify-between space-x-24">
+          <span>
+            <select id="to-currency" className="p-1 px-2 rounded-sm bg-lime-400 text-black/60 outline-none">
+              <option value="eu">EU</option>
+              <option value="us">US</option>
+            </select>
+          </span>
+
+          <span className="text-xl text-lime-400 hover:text-lime-500 transition-colors">
+            <Link href="/">Login</Link>
+          </span>
+        </div>
+
       </nav>
     </div>
   )
