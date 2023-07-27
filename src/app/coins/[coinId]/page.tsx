8fcd254f2b3a7021d43191f813a1199d2d94f2cb
Vladimir Vlayco <vladaj00@gmail.com>
2023-07-27 11:39:58 +0200

    minor changes

--- src/app/coins/[coinId]/page.tsx
+++ src/app/coins/[coinId]/page.tsx
@@ -1,6 +1,8 @@
 const Coin = ({ params }: { params: { coinId: string } }) => {
   return (
-    <div>Coin Page {params.coinId}</div>
+    <main className='flex-1 text-yellow-100'>
+      <section className='container mx-auto'>Coin page {params.coinId}</section>
+    </main>
   )
 }
 
